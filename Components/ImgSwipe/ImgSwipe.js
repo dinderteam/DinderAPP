1cdd6a488d043bfce441ef2b207608cdfe890f95
Simon Tekeste <st.tekeste@gmail.com>
2018-12-13 06:17:36 -0800

    react navigation worksgit add * git add *

---
+++ Components/ImgSwipe/ImgSwipe.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Image, StyleSheet } from 'react-native';
+import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures';
+
+class ImgSwipe extends Component {
+        
+    // onSwipeUp(gestureState) {
+    //     this.setState({ myText: 'You swiped up!' }); //im going to use this for instant checkout 
+    // }
+
+    // onSwipeDown(gestureState) {
+    //     this.setState({ myText: 'You swiped down!' });  // seeing reviews and more information about the businesss 
+    // }                                                   // such as more images
+
+    onSwipeLeft(gestureState) {
+        console.log("leftttt!!!!!!!!")
+    }
+
+    onSwipeRight(gestureState) {
+        console.log("right!!!!!!!!!!!")
+        
+    }
+
+
+    handleLangChange = (gestureState) => {
+        this.props.updateChange(gestureState);
+    }
+
+    onSwipe(gestureName, gestureState,) {
+        const { SWIPE_UP, SWIPE_DOWN, SWIPE_LEFT, SWIPE_RIGHT } = swipeDirections;
+        //this.setState({ gestureName: gestureName });
+        switch (gestureName) {
+            case SWIPE_UP:
+        
+                break;
+            case SWIPE_DOWN:
+                break;
+            case SWIPE_LEFT:
+                this.handleLangChange(gestureName)
+                break;
+            case SWIPE_RIGHT:
+                this.handleLangChange(gestureName)
+                break;
+        }
+    }
+
+    render() {
+        
+        const config = {
+            velocityThreshold: 0.3,
+            directionalOffsetThreshold: 80
+        };
+
+        return (
+             
+            <GestureRecognizer
+                onSwipe={(direction,change) => this.onSwipe(direction,change)}
+                // onSwipeUp={(state) => this.onSwipeUp(state)}
+                // onSwipeDown={(state) => this.onSwipeDown(state)}
+                onSwipeLeft={() => this.onSwipeLeft()}
+                onSwipeRight={() => this.onSwipeRight()}
+                config={config}   
+            >
+                <Image style={styles.position} source={{ uri: this.props.currentImage }} /> 
+            </GestureRecognizer>
+            
+        );
+    }
+}
+const styles = StyleSheet.create({
+    position: {
+        width: 375,
+        height: 450,  
+    }
+
+});
+
+export default ImgSwipe;
---
+++ Pages/HomePage.js
@@ -0,0 +1,161 @@
+import React from "react";
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import ImgSwipe from "../Components/ImgSwipe/ImgSwipe.js"
+
+export default class HomePage extends React.Component {
+    state = {
+        swipeState: null,
+        currentName: null,
+        currentImage: 'https://i1.wp.com/www.foot.com/wp-content/uploads/2017/03/placeholder.gif?ssl=1',
+        currentPrice: null,
+        currentRating: null,
+        currentphone: null,
+        currentAddress: null,
+        currentUrl: null,
+        currentReview: null,
+    }
+
+    componentWillMount() {
+        var { height, width } = Dimensions.get('window');
+        console.log(height)
+        console.log(width)
+    }
+
+    componentDidMount() {
+        fetch("https://api.yelp.com/v3/businesses/search?term=food&radius=16093&location=oakland", {
+            method: "GET",
+            headers: new Headers({
+                "Content-type": "application/json",
+                'Authorization': ' Bearer ' + "EgNHeojg_ryrKUYzlgCaPMXU7i60GOR-Yy1qxnoYvIDNM8OEq1bfq1a5cbuiExw94-oDF86cKIGfZI73iQoXsxZYndshHdSCeqUMjCi1C-KqdY1jA2Rkw5O4OQWwWnYx",
+            }),
+
+        })
+            .then(response => response.json())
+            .then(info => {
+                this.pushIntoDB(info);
+            })
+            .catch((error) => { console.warn("Unable to connect to network.") })
+    }
+
+    pushIntoDB = (info) => {
+
+        fetch('http://localhost:8080/data/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(info),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Got this back', data);
+            })
+            .catch((error) => { console.warn("Unable to push to DB") })
+    }
+
+    onSwipeChange = () => {
+        fetch('http://localhost:8080/', {
+            method: "GET",
+            headers: { 'Content-Type': 'application/json' },
+        })
+            .then(Response => Response.json())
+            .then(result => {
+
+                this.setState({
+                    currentName: result[0]['name'],
+                    currentImage: result[0]['image_url'],
+                    currentPrice: result[0]['price'],
+                    currentRating: result[0]['rating'],
+                    currentphone: result[0]['display_phone'],
+                    currentAddress: result[0]['location']['display_address'],
+                    currentUrl: result[0]['url'],
+                    currentReview: result[0]['review_count'],
+                })
+            })
+            .catch((error) => { console.warn("Unable to fetch Data") });
+
+    }
+
+
+    onChangeHandler = (direction) => {
+
+        if (direction === "SWIPE_LEFT") {
+            this.onSwipeChange()
+
+        } else if (direction === "SWIPE_RIGHT") {
+            this.onSwipeChange()
+
+        }
+
+    }
+
+    render() {
+
+        let currentImageMarker = this.state.currentImage;
+        if (this.state.currentImage) {
+            currentImageMarker = this.state.currentImage
+        }
+
+        return (
+
+            <View style={styles.mainContainer}>
+                <View style={styles.labelContainer}>
+                    <Text style={styles.label}> DINDER </Text>
+                </View>
+
+                <View style={styles.restName}>
+                    <Text style={styles.label}>{"Name:" + this.state.currentName}</Text>
+                </View>
+
+                <View style={styles.imgContainer}>
+
+                    <View style={styles.dataView}>
+                        <Text style={styles.price}>{"Price:" + this.state.currentPrice}</Text>
+                        <Text style={styles.rating}>{"Rating:" + this.state.currentRating}</Text>
+                    </View>
+
+                    <View>
+                        <ImgSwipe currentImage={currentImageMarker} updateChange={this.onChangeHandler} />
+                    </View>
+
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+    },
+    labelContainer: {
+        flex: 1,
+        flexDirection: "column",
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: "red",
+    },
+    label: {
+        fontSize: 36,
+        textAlign: "center",
+
+    },
+    restName: {
+        marginTop: 10,
+        marginBottom: 10,
+        flexDirection: "column",
+    },
+    dataView: {
+        flexDirection: "row",
+    },
+    imgContainer: {
+        flex: 6,
+        alignItems: "center",
+    },
+    price: {
+        flex: 1,
+        textAlign: "left",
+    },
+    rating: {
+        flex: 1,
+        textAlign: "right",
+    }
+});
